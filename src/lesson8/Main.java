796267c9eca9b3d965c77345d644e06e39108f55
cuirros <rahmatullaev04@gmail.com>
2025-07-17 19:10:26 +0500

    реализовал метод который пишет задом наперед строку

--- src/lesson8/Main.java
+++ src/lesson8/Main.java
@@ -3,6 +3,9 @@ package lesson8;
 public class Main {
     public static void main(String[] args) {
         System.out.println(findSymbolOccurance("Heellllpp Meee1111", '1'));
+        String first = "hello";
+        String reversed = stringReverse(first);
+        System.out.println(reversed);
     }
 
     static int findSymbolOccurance(String text, char symbol) {
@@ -13,5 +16,12 @@ public class Main {
             }
         }
         return counter;
+    }
+    static String stringReverse(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+        return reverse;
     }
 }
